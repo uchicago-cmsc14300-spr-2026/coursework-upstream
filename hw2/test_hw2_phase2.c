ac76398fb94350b4aa81cd99fef81876c5aaf737
Adam Shaw <ams@cs.uchicago.edu>
2026-04-19 11:10:11 -0500

    added .timeout=2 to tests to handle infinite loops as needed

--- hw2/test_hw2_phase2.c
+++ hw2/test_hw2_phase2.c
@@ -4,62 +4,62 @@
 
 // ======== tests
 
-Test(hw2_count_color, count_color00)
+Test(hw2_count_color, count_color00, .timeout=2)
 {
   rgb colors[] = {0X000000, 0XF00000, 0XF00000};
   cr_assert(count_color(0XF00000, colors, 3)==2);
 }
 
-Test(hw2_count_color, count_color01)
+Test(hw2_count_color, count_color01, .timeout=2)
 {
   rgb colors[] = {0X000000, 0XFFFFFF, 0XFF0000, 0X00FF00, 0X0000FF};
   cr_assert(count_color(0XFFFF00, colors, 5)==0);
 }
 
-Test(hw2_count_color, count_color02)
+Test(hw2_count_color, count_color02, .timeout=2)
 {
   rgb colors[] = {0X000000, 0XFFFFFF, 0XFF0000, 0X00FF00, 0X0000FF};
   cr_assert(count_color(0XFF0000, colors, 5)==1);
 }
 
-Test(hw2_count_color, count_color03)
+Test(hw2_count_color, count_color03, .timeout=2)
 {
   rgb colors[] = {0X000000, 0XFFFFFF, 0XFF0000, 0X00FF00, 0X0000FF};
   cr_assert(count_color(0X0000FF, colors, 5)==1);
 }
 
-Test(hw2_count_color, count_color04)
+Test(hw2_count_color, count_color04, .timeout=2)
 {
   rgb colors[] = {0X000000, 0XFFFFFF, 0XFF0000, 0X00FF00, 0X0000FF};
   cr_assert(count_color(0XFF0000, colors, 5)==1);
 }
 
-Test(hw2_count_color, count_color05)
+Test(hw2_count_color, count_color05, .timeout=2)
 {
   rgb colors[] = {0X000000, 0XFFFFFF, 0XFF0000, 0XFFFFFF, 0X0000FF};
   cr_assert(count_color(0XFFFFFF, colors, 5)==2);
 }
 
-Test(hw2_count_color, count_color06)
+Test(hw2_count_color, count_color06, .timeout=2)
 {
   rgb colors[] = {0X000000, 0XFFFFFF, 0X000000, 0X00FF00, 0X000000};
   cr_assert(count_color(0X000000, colors, 5)==3);
 }
 
-Test(hw2_count_color, count_color07)
+Test(hw2_count_color, count_color07, .timeout=2)
 {
   rgb colors[] = {0XFF00FF, 0XFF00FF, 0XFF00FF, 0X00FF00, 0XFF00FF};
   cr_assert(count_color(0XFF00FF, colors, 5)==4);
 }
 
-Test(hw2_count_color, count_color08)
+Test(hw2_count_color, count_color08, .timeout=2)
 {
   rgb colors[] = {0XFF00FF, 0XFF00FF, 0XFF00FF, 0XFF00FF, 0XFF00FF};
   cr_assert(count_color(0XFF00FF, colors, 5)==5);
 }
 
 
-Test(hw2_negate_colors, negate_colors00)
+Test(hw2_negate_colors, negate_colors00, .timeout=2)
 {
   rgb colors[]  = {0XFF00FF, 0X00FF00};
   rgb negated[] = {0X00FF00, 0XFF00FF};
@@ -67,7 +67,7 @@ Test(hw2_negate_colors, negate_colors00)
   cr_assert(colors[0]==negated[0] && colors[1]==negated[1]);      
 }
 
-Test(hw2_negate_colors, negate_colors01)
+Test(hw2_negate_colors, negate_colors01, .timeout=2)
 {
   rgb colors[]  = {0X732AB4};
   rgb negated[] = {0X8CD54B};
@@ -75,7 +75,7 @@ Test(hw2_negate_colors, negate_colors01)
   cr_assert(colors[0]==negated[0]);
 }
 
-Test(hw2_negate_colors, negate_colors02)
+Test(hw2_negate_colors, negate_colors02, .timeout=2)
 {
   rgb colors[]  = {0XC64B20, 0XA913E4, 0X17645F};
   rgb negated[] = {0X39B4DF, 0X56EC1B, 0XE89BA0};
@@ -84,7 +84,7 @@ Test(hw2_negate_colors, negate_colors02)
             colors[2]==negated[2]);
 }
 
-Test(hw2_negate_colors, negate_colors03)
+Test(hw2_negate_colors, negate_colors03, .timeout=2)
 {
   rgb colors[]  = {0X7910AE, 0X927D19, 0X07229D, 0XFEA0D6};
   rgb negated[] = {0X86EF51, 0X6D82E6, 0XF8DD62, 0X015F29};
@@ -94,7 +94,7 @@ Test(hw2_negate_colors, negate_colors03)
 }
 
 
-Test(hw2_stretch, stretch00)
+Test(hw2_stretch, stretch00, .timeout=2)
 {
   rgb colors[] = {0, 0XF0F0F0};
   rgb *s = stretch(colors, 2);
@@ -102,7 +102,7 @@ Test(hw2_stretch, stretch00)
   free(s);
 }
 
-Test(hw2_stretch, stretch01)
+Test(hw2_stretch, stretch01, .timeout=2)
 {
   rgb colors[] = {0xA713D1};
   rgb *s = stretch(colors, 1);
@@ -110,7 +110,7 @@ Test(hw2_stretch, stretch01)
   free(s);
 }
 
-Test(hw2_stretch, stretch02)
+Test(hw2_stretch, stretch02, .timeout=2)
 {
   rgb colors[] = {0X961A44, 0X213384, 0XAA0DFE};
   rgb *s = stretch(colors, 3);
@@ -119,7 +119,7 @@ Test(hw2_stretch, stretch02)
   free(s);
 }
 
-Test(hw2_stretch, stretch03)
+Test(hw2_stretch, stretch03, .timeout=2)
 {
   rgb colors[] = {0X961A44, 0X213384, 0XAA0DFE, 0X94F7E6};
   rgb *s = stretch(colors, 4);
@@ -129,7 +129,7 @@ Test(hw2_stretch, stretch03)
   free(s);
 }
 
-Test(hw2_stretch, stretch04)
+Test(hw2_stretch, stretch04, .timeout=2)
 {
   rgb colors[] = {0X961A44, 0X213384, 0XAA0DFE, 0X94F7E6, 0X9E6F2C};
   rgb *s = stretch(colors, 5);
@@ -141,56 +141,56 @@ Test(hw2_stretch, stretch04)
 }
 
 
-Test(hw2_min_blue, min_blue00)
+Test(hw2_min_blue, min_blue00, .timeout=2)
 {
   rgb colors[] = {0XFFEEBB, 0XFFDDAA};
   cr_assert(min_blue(colors,2)==0XAA);
 }
 
-Test(hw2_min_blue, min_blue01)
+Test(hw2_min_blue, min_blue01, .timeout=2)
 {
   rgb colors[] = {0XAB00C9, 0X610024, 0X7A00B4, 0X23001A, 0X7B0023};
   cr_assert(min_blue(colors,5)==0X1A);
 }
 
-Test(hw2_min_blue, min_blue02)
+Test(hw2_min_blue, min_blue02, .timeout=2)
 {
   rgb colors[] = {0XABFF19, 0X61FF24, 0X7AFFB4, 0X23FF1A, 0X7BFF23};
   cr_assert(min_blue(colors,5)==0X19);
 }
 
-Test(hw2_min_blue, min_blue03)
+Test(hw2_min_blue, min_blue03, .timeout=2)
 {
   rgb colors[] = {0XAB00C9, 0X61FF14, 0X7A00B4, 0X23001A, 0X7B00F3};
   cr_assert(min_blue(colors,5)==0X14);
 }
 
-Test(hw2_min_blue, min_blue04)
+Test(hw2_min_blue, min_blue04, .timeout=2)
 {
   rgb colors[] = {0XAB6E11, 0X611224, 0X7A86B4, 0X23871A, 0X7B2223};
   cr_assert(min_blue(colors,5)==0X11);
 }
 
-Test(hw2_min_blue, min_blue05)
+Test(hw2_min_blue, min_blue05, .timeout=2)
 {
   rgb colors[] = {0XAB00C9, 0X610724, 0X7A1910, 0X230D1A, 0X7B1A23};
   cr_assert(min_blue(colors,5)==0X10);
 }
 
-Test(hw2_min_blue, min_blue06)
+Test(hw2_min_blue, min_blue06, .timeout=2)
 {
   rgb colors[] = {0XAB7323, 0X619723, 0X7A2223, 0X239723, 0X7B8F23};
   cr_assert(min_blue(colors,5)==0X23);
 }
 
-Test(hw2_min_blue, min_blue07)
+Test(hw2_min_blue, min_blue07, .timeout=2)
 {
   rgb colors[] = {0XAB00C9, 0XAB1824, 0X7A00B4, 0X23001A, 0X7B001A};
   cr_assert(min_blue(colors,5)==0X1A);
 }
 
 
-Test(hw2_connect_rgb_arrays, connect_rgb_arrays00)
+Test(hw2_connect_rgb_arrays, connect_rgb_arrays00, .timeout=2)
 {
   rgb colors1[] = {0, 0X0000BB};
   rgb colors2[] = {0XFF0000};
@@ -201,7 +201,7 @@ Test(hw2_connect_rgb_arrays, connect_rgb_arrays00)
   free(c);
 }
 
-Test(hw2_connect_rgb_arrays, connect_rgb_arrays01)
+Test(hw2_connect_rgb_arrays, connect_rgb_arrays01, .timeout=2)
 {
   rgb colors1[] = {0XA9B623};
   rgb colors2[] = {0X148296};
@@ -211,7 +211,7 @@ Test(hw2_connect_rgb_arrays, connect_rgb_arrays01)
   free(c);
 }
 
-Test(hw2_connect_rgb_arrays, connect_rgb_arrays02)
+Test(hw2_connect_rgb_arrays, connect_rgb_arrays02, .timeout=2)
 {
   rgb colors1[] = {0X912C34};
   rgb colors2[] = {0XA601CB, 0X22341A};
@@ -222,7 +222,7 @@ Test(hw2_connect_rgb_arrays, connect_rgb_arrays02)
   free(c);
 }
 
-Test(hw2_connect_rgb_arrays, connect_rgb_arrays03)
+Test(hw2_connect_rgb_arrays, connect_rgb_arrays03, .timeout=2)
 {
   rgb colors1[] = {0X994328, 0X221827, 0X16A409};
   rgb colors2[] = {0XA49BCD, 0X1664A8, 0X9193AB, 0X774A8B};
@@ -237,7 +237,7 @@ Test(hw2_connect_rgb_arrays, connect_rgb_arrays03)
   free(c);
 }
 
-Test(hw2_connect_rgb_arrays, connect_rgb_arrays04)
+Test(hw2_connect_rgb_arrays, connect_rgb_arrays04, .timeout=2)
 {
   rgb colors1[] = {0X994328, 0X221827, 0X16A409, 0XA49BCD};
   rgb colors2[] = {0X1664A8, 0X9193AB, 0X774A8B};
@@ -252,7 +252,7 @@ Test(hw2_connect_rgb_arrays, connect_rgb_arrays04)
   free(c);
 }
 
-Test(hw2_connect_rgb_arrays, connect_rgb_arrays05)
+Test(hw2_connect_rgb_arrays, connect_rgb_arrays05, .timeout=2)
 {
   rgb colors1[] = {0X994328, 0X221827, 0X16A409, 0XA49BCD};
   rgb colors2[] = {0X1664A8, 0X9193AB, 0X774A8B, 0X97338E};
@@ -278,7 +278,7 @@ Test(hw2_sift_up, sift_up00)
 	    dates[2]==20250201000000);
 }
 
-Test(hw2_sift_up, sift_up01)
+Test(hw2_sift_up, sift_up01, .timeout=2)
 {
   timestamp dates[] = {20250201000000, 20250301000000, 20250201000000};
   sift_up(dates, 1);
@@ -287,7 +287,7 @@ Test(hw2_sift_up, sift_up01)
       dates[2]==20250201000000);
 }
 
-Test(hw2_sift_up, sift_up02)
+Test(hw2_sift_up, sift_up02, .timeout=2)
 {
   timestamp dates[] = {20250201000000, 20250203000000, 20250301000000,
     20250204000000, 20250205000000, 20250302000000, 20250303000000,
@@ -303,7 +303,7 @@ Test(hw2_sift_up, sift_up02)
             dates[7]==20250204000000);
 }
 
-Test(hw2_sift_up, sift_up03)
+Test(hw2_sift_up, sift_up03, .timeout=2)
 {
   timestamp dates[] = {20250202000000, 20250203000000, 20250301000000,
     20250204000000, 20250205000000, 20250302000000, 20250303000000,
@@ -336,7 +336,7 @@ Test(hw2_sift_down, sift_down00)
             dates[7]==20250205000000);
 }
 
-Test(hw2_sift_down, sift_down01)
+Test(hw2_sift_down, sift_down01, .timeout=2)
 {
   timestamp dates[] = {20250202000000, 20250201000000, 20250301000000,
     20250203000000, 20250204000000, 20250302000000, 20250303000000,
@@ -352,7 +352,7 @@ Test(hw2_sift_down, sift_down01)
             dates[7]==20250205000000);
 }
 
-Test(hw2_sift_down, sift_down02)
+Test(hw2_sift_down, sift_down02, .timeout=2)
 {
   timestamp dates[] = {20250202000000, 20250301000000, 20250201000000,
     20250302000000, 20250303000000, 20250203000000, 20250204000000,
@@ -368,7 +368,7 @@ Test(hw2_sift_down, sift_down02)
             dates[7]==20250305000000);
 }
 
-Test(hw2_sift_down, sift_down03)
+Test(hw2_sift_down, sift_down03, .timeout=2)
 {
   timestamp dates[] = {20250203000000, 20250201000000, 20250301000000,
     20250202000000, 20250204000000, 20250302000000, 20250303000000,
@@ -384,7 +384,7 @@ Test(hw2_sift_down, sift_down03)
             dates[7]==20250205000000);
 }
 
-Test(hw2_sift_down, sift_down04)
+Test(hw2_sift_down, sift_down04, .timeout=2)
 {
   timestamp dates[] = {20250205000000, 20250201000000, 20250301000000,
     20250202000000, 20250204000000, 20250302000000, 20250303000000,
@@ -400,7 +400,7 @@ Test(hw2_sift_down, sift_down04)
             dates[7]==20250205000000);
 }
 
-Test(hw2_sift_down, sift_down05)
+Test(hw2_sift_down, sift_down05, .timeout=2)
 {
   timestamp dates[] = {20250201000000, 20250202000000, 20250301000000,
     20250203000000, 20250204000000, 20250302000000, 20250303000000,
@@ -416,7 +416,7 @@ Test(hw2_sift_down, sift_down05)
             dates[7]==20250205000000);
 }
 
-Test(hw2_sift_down, sift_down06)
+Test(hw2_sift_down, sift_down06, .timeout=2)
 {
   timestamp dates[] = {20250201000000, 20250203000000, 20250301000000,
     20250202000000, 20250204000000, 20250302000000, 20250303000000,
@@ -432,7 +432,7 @@ Test(hw2_sift_down, sift_down06)
             dates[7]==20250205000000);
 }
 
-Test(hw2_sift_down, sift_down07)
+Test(hw2_sift_down, sift_down07, .timeout=2)
 {
   timestamp dates[] = {20250201000000, 20250203000000, 20250301000000,
     20250204000000, 20250202000000, 20250302000000, 20250303000000,
@@ -500,14 +500,14 @@ Test(hw2_heapsort_dates, heapsort_dates00)
 	    dates[2]==20200101000000);
 }
 
-Test(hw2_heapsort_dates, heapsort_dates01)
+Test(hw2_heapsort_dates, heapsort_dates01, .timeout=2)
 {
   timestamp dates[] = {20240314000000};
   heapsort_dates(dates,1);
   cr_assert(dates[0]==20240314000000);
 }
 
-Test(hw2_heapsort_dates, heapsort_dates02)
+Test(hw2_heapsort_dates, heapsort_dates02, .timeout=2)
 {
   timestamp dates[] = {20240314000000, 20230404000000};
   heapsort_dates(dates,2);
@@ -515,7 +515,7 @@ Test(hw2_heapsort_dates, heapsort_dates02)
             dates[1]==20240314000000);
 }
 
-Test(hw2_heapsort_dates, heapsort_dates03)
+Test(hw2_heapsort_dates, heapsort_dates03, .timeout=2)
 {
   timestamp dates[] = {20250506123456, 20211231081005, 20230706001500,
                       20221008231024};
@@ -526,7 +526,7 @@ Test(hw2_heapsort_dates, heapsort_dates03)
             dates[3]==20250506123456);
 }
 
-Test(hw2_heapsort_dates, heapsort_dates04)
+Test(hw2_heapsort_dates, heapsort_dates04, .timeout=2)
 {
   timestamp dates[] = {20210203040506, 20220304050607, 20230405060708,
                       20240506070809, 20250607080910, 20260708091011,
@@ -542,7 +542,7 @@ Test(hw2_heapsort_dates, heapsort_dates04)
             dates[7]==20280910111213);
 }
 
-Test(hw2_heapsort_dates, heapsort_dates05)
+Test(hw2_heapsort_dates, heapsort_dates05, .timeout=2)
 {
   timestamp dates[] = {20280910111213, 20270809101112, 20260708091011,
     20250607080910, 20240506070809, 20230405060708,
@@ -558,7 +558,7 @@ Test(hw2_heapsort_dates, heapsort_dates05)
             dates[7]==20280910111213);
 }
 
-Test(hw2_heapsort_dates, heapsort_dates06)
+Test(hw2_heapsort_dates, heapsort_dates06, .timeout=2)
 {
   timestamp dates[] = {20220304050607, 20260708091011, 20280910111213,
                       20230405060708, 20240506070809, 20250607080910,
@@ -574,7 +574,7 @@ Test(hw2_heapsort_dates, heapsort_dates06)
             dates[7]==20280910111213);
 }
 
-Test(hw2_heapsort_dates, heapsort_dates07)
+Test(hw2_heapsort_dates, heapsort_dates07, .timeout=2)
 {
   timestamp dates[] = {20220304050607, 20260708091011, 20280910111213,
                       20230405060708, 20240506070809, 20230405060708,
@@ -616,7 +616,7 @@ Test(hw2_distinct_dates, distinct_dates00)
   free(ds);
 }
 
-Test(hw2_distinct_dates, distinct_dates01)
+Test(hw2_distinct_dates, distinct_dates01, .timeout=2)
 {
   timestamp dates[] = {20160812231907};
   int n;
@@ -625,7 +625,7 @@ Test(hw2_distinct_dates, distinct_dates01)
   free(ds);
 }
 
-Test(hw2_distinct_dates, distinct_dates02)
+Test(hw2_distinct_dates, distinct_dates02, .timeout=2)
 {
   timestamp dates[] = {20180403221417, 20120618081203};
   int n;
@@ -634,7 +634,7 @@ Test(hw2_distinct_dates, distinct_dates02)
   free(ds);
 }
 
-Test(hw2_distinct_dates, distinct_dates03)
+Test(hw2_distinct_dates, distinct_dates03, .timeout=2)
 {
   timestamp dates[] = {20180403221417, 20120618081203, 20201201031824};
   int n;
@@ -644,7 +644,7 @@ Test(hw2_distinct_dates, distinct_dates03)
   free(ds);
 }
 
-Test(hw2_distinct_dates, distinct_dates04)
+Test(hw2_distinct_dates, distinct_dates04, .timeout=2)
 {
   timestamp dates[] = {20120618081203, 20120618081203, 20201201031824};
   int n;
@@ -653,7 +653,7 @@ Test(hw2_distinct_dates, distinct_dates04)
   free(ds);
 }
 
-Test(hw2_distinct_dates, distinct_dates05)
+Test(hw2_distinct_dates, distinct_dates05, .timeout=2)
 {
   timestamp dates[] = {20180403221417, 20120618081203, 20180403221417};
   int n;
@@ -662,7 +662,7 @@ Test(hw2_distinct_dates, distinct_dates05)
             
 }
 
-Test(hw2_distinct_dates, distinct_dates06)
+Test(hw2_distinct_dates, distinct_dates06, .timeout=2)
 {
   timestamp dates[] = {20180403221417, 20120618081203, 20120618081203};
   int n;
@@ -671,7 +671,7 @@ Test(hw2_distinct_dates, distinct_dates06)
   free(ds);
 }
 
-Test(hw2_distinct_dates, distinct_dates07)
+Test(hw2_distinct_dates, distinct_dates07, .timeout=2)
 {
   timestamp dates[] = {20180403221417, 20180403221417, 20180403221417};
   int n;
@@ -706,7 +706,7 @@ Test(hw2_weekdays, weekdays00)
   free(w);
 }
 
-Test(hw2_weekdays, weekdays01)
+Test(hw2_weekdays, weekdays01, .timeout=2)
 {
   int n;
   timestamp *w = weekdays(20230517123456, 20230513123456, &n);
@@ -714,7 +714,7 @@ Test(hw2_weekdays, weekdays01)
   (void)w; // nonsense operation to silence compiler warning
 }
 
-Test(hw2_weekdays, weekdays02)
+Test(hw2_weekdays, weekdays02, .timeout=2)
 {
   int n;
   timestamp *w = weekdays(20230520123456, 20230521123456, &n);
@@ -722,7 +722,7 @@ Test(hw2_weekdays, weekdays02)
   (void)w; // nonsense operation to silence compiler warning
 }
 
-Test(hw2_weekdays, weekdays03)
+Test(hw2_weekdays, weekdays03, .timeout=2)
 {
   int n;
   timestamp *w = weekdays(20230521123456, 20230527123456, &n);
@@ -731,7 +731,7 @@ Test(hw2_weekdays, weekdays03)
   free(w);
 }
 
-Test(hw2_weekdays, weekdays04)
+Test(hw2_weekdays, weekdays04, .timeout=2)
 {
   int n;
   timestamp *w = weekdays(20230520123456, 20230528123456, &n);
@@ -740,7 +740,7 @@ Test(hw2_weekdays, weekdays04)
   free(w);
 }
 
-Test(hw2_weekdays, weekdays05)
+Test(hw2_weekdays, weekdays05, .timeout=2)
 {
   int n;
   timestamp *w = weekdays(20230522123456, 20230526123456, &n);
@@ -749,7 +749,7 @@ Test(hw2_weekdays, weekdays05)
   free(w);
 }
 
-Test(hw2_weekdays, weekdays06)
+Test(hw2_weekdays, weekdays06, .timeout=2)
 {
   int n;
   timestamp *w = weekdays(20230523123456, 20230525123456, &n);
@@ -758,7 +758,7 @@ Test(hw2_weekdays, weekdays06)
   free(w);
 }
 
-Test(hw2_weekdays, weekdays07)
+Test(hw2_weekdays, weekdays07, .timeout=2)
 {
   int n;
   timestamp *w = weekdays(20180201234507, 20180228234507, &n);
@@ -780,7 +780,7 @@ Test(hw2_mondays, mondays00)
   free(w);
 }
 
-Test(hw2_mondays, mondays01)
+Test(hw2_mondays, mondays01, .timeout=2)
 {
   int n;
   timestamp *w = mondays(20230517123456, 20230518123456, &n);
@@ -788,7 +788,7 @@ Test(hw2_mondays, mondays01)
   (void)w; // nonsense operation to silence compiler warning
 }
 
-Test(hw2_mondays, mondays02)
+Test(hw2_mondays, mondays02, .timeout=2)
 {
   int n;
   timestamp *w = mondays(20230501123456, 20230531123456, &n);
@@ -797,7 +797,7 @@ Test(hw2_mondays, mondays02)
   free(w);
 }
 
-Test(hw2_mondays, mondays03)
+Test(hw2_mondays, mondays03, .timeout=2)
 {
   int n;
   timestamp *w = mondays(20230522123456, 20230522123456, &n);
@@ -806,7 +806,7 @@ Test(hw2_mondays, mondays03)
   free(w);
 }
 
-Test(hw2_mondays, mondays04)
+Test(hw2_mondays, mondays04, .timeout=2)
 {
   int n;
   timestamp *w = mondays(20180201234507, 20180228234507, &n);
@@ -828,7 +828,7 @@ Test(hw2_primes, primes00)
     free(np);
 }
 
-Test(hw2_primes, primes01)
+Test(hw2_primes, primes01, .timeout=2)
 {
   int n;
   unsigned int a[] = {3};
@@ -838,7 +838,7 @@ Test(hw2_primes, primes01)
   free(np);
 }
 
-Test(hw2_primes, primes02)
+Test(hw2_primes, primes02, .timeout=2)
 {
   int n;
   unsigned int a[] = {24};
@@ -847,7 +847,7 @@ Test(hw2_primes, primes02)
   (void)np; // nonsense operation to silence compiler warning
 }
 
-Test(hw2_primes, primes03)
+Test(hw2_primes, primes03, .timeout=2)
 {
   int n;
   unsigned int a[] = {2,3,5,7,11,13,17,19,23,29};
@@ -866,7 +866,7 @@ Test(hw2_primes, primes03)
   free(np);
 }
 
-Test(hw2_primes, primes04)
+Test(hw2_primes, primes04, .timeout=2)
 {
   int n;
   unsigned int a[] = {4,9,15,21,25,28};
